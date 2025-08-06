2b471cf8c9a7d573b5a85d6f4b0e2963b3622b1e
Muhammadkhuja <muhammadxuja9@gmail.com>
2025-08-06 16:58:21 +0500

    same developer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<"postgres">("DB_CONNECTION"),
+        host: config.get<string>("DB_HOST"),
+        username: config.get<string>("DB_USERNAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        port: config.get<number>("DB_PORT"),
+        database: config.get<string>("DB_NAME"),
+        entities: [__dirname + "dist/**/*.entity{.ts,.js}"],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: false,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
