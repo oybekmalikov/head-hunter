cbc546ffae9eac21349bbcb6c3a8cc5544dcb1c0
oybekmalikov <162029195+oybekmalikov@users.noreply.github.com>
2025-08-06 11:10:44 +0300

    params

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
+import { AppModule } from "./app.module";
+// import { ErrorHandler } from "./common/error_handling/errorhandler";
+// import { LoggerService } from "./common/logger/logger.service";
 async function start() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	try {
+		const PORT = process.env.PORT || 3030;
+		const HOST = process.env.HOST || "localhost";
+		const app = await NestFactory.create(AppModule);
+		app.useGlobalPipes(new ValidationPipe());
+		app.setGlobalPrefix("api");
+		app.use(cookieParser());
+		app.enableCors({
+			origin: '*',
+			methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+			credentials: false,
+		});
+		const config = new DocumentBuilder()
+			.setTitle("Private Hospital Project")
+			.setDescription("Discount REST API")
+			.setVersion("1.0")
+			.addTag("Nest JS", "Swagger")
+			.addTag("Validation", "Guard")
+			.addBearerAuth()
+			.build();
+		const document = SwaggerModule.createDocument(app, config);
+		SwaggerModule.setup("api/docs", app, document);
+		// const logger = app.get(LoggerService);
+		// app.useGlobalFilters(new ErrorHandler(logger));
+		await app.listen(PORT, () => {
+			console.log(`Server started on http://${HOST}:${PORT}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
 }
 start();
